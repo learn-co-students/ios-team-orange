d33a2c3d4afe043415f9d67e8b28f659aaccd9f3
Michael Florez <mike_florez@ymail.com>
2017-04-03 15:58:39 -0400

    add User model

--- TeamOrange/User.swift
+++ TeamOrange/User.swift
@@ -11,30 +11,37 @@ import Foundation
 class User {
     
     var userName: String
+    var profPic: String?
+    var gender: Gender?
     let birthDate: String?
-    var favLocations: [Location]?
-    var favSport: Sport?
-    var friends: [User]?
-    var games: [Game]?
-    var gender: Gender
-    var homeField: Location?
-    var homeTown: String
-    var profPic: String
-    var teams: [Team]?
+    var homeTown: String?
     var zipCode: Int
+    var favSport: Sport?
+    var homeField: Location?
+    var favLocations: [Location]?
+    var friends: [User]?
+    var teams: [Team]?
+    var games: [Game]?
     
     init?(userDict: [String:Any]) {
         guard let userName = userDict["userName"] as? String else {print("Username not valid"); return nil}
         self.userName = userName
-        self.birthDate = userDict["birthDate"] as? String
-        self.favLocations = userDict["favLocations"] as? [Location] //TODO: Figure out how to unwrap
-        self.favSport = userDict["favSport"] as? String
-        self.friends = userDict["friends"] as? [User]
-        self.games = userDict["games"] as? [Game]
-        self.gender = userDict["gender"] as? Bool
-        self.homeField = userDict["gender"] as? String
-        self.homeTown = userDict["homeTown"] as? String
-        self.profPic = userDict["profPic"] as? String
+        
+        self.profPic = userDict["profPic"] as? String ?? "Profile pic URL not valid"
+        
+        guard let gender = userDict["gender"] as? Gender else {print("Gender not valid");return nil}
+        self.gender = gender
+        
+        self.birthDate = userDict["birthDate"] as? String ?? "Birthdate not valid"
+        
+        guard let homeTown = userDict["homeTown"] as? String else {print("Hometown not valid");return nil}
+        self.homeTown = homeTown
+        
+        self.zipCode = userDict["zipCode"] as? Int ?? 0
+        
+        guard let favSport = userDict["favSport"] as? Sport else {print("Favorite sport not found"); return nil}
+        self.favSport = favSport
+        
+        self.homeField = userDict["homeField"] as? Location ?? nil
     }
-    
 }
