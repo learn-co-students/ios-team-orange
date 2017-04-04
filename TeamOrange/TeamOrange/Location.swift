221bfd378f66e6a3f2b7f8de98c1a63d830de35c
Michael Florez <mike_florez@ymail.com>
2017-04-04 11:21:14 -0400

    removed mainstory filebase name in info.plist & created view in appdelegate

--- TeamOrange/TeamOrange/Location.swift
+++ TeamOrange/TeamOrange/Location.swift
@@ -1,25 +1,25 @@
+////
+////  Location.swift
+////  TeamOrange
+////
+////  Created by William Brancato on 4/3/17.
+////  Copyright © 2017 William Brancato. All rights reserved.
+////
 //
-//  Location.swift
-//  TeamOrange
-//
-//  Created by William Brancato on 4/3/17.
-//  Copyright © 2017 William Brancato. All rights reserved.
-//
-
-import Foundation
-import MapKit
-class Location {
-    let id: String
-    let latitude: Double?
-    let longitude: Double?
-    let name: String
-    var games: [Game]?
-    let address: String
-    var users:[User]?
-    
-    init?(id: String, dict: [String: Any]) {
-        self.id = id
-        guard let addr = dict["address"], let name = dict["name"] else {return nil}
-        
-    }
-}
+//import Foundation
+//import MapKit
+//class Location {
+//    let id: String
+//    let latitude: Double?
+//    let longitude: Double?
+//    let name: String
+//    var games: [Game]?
+//    let address: String
+//    var users:[User]?
+//    
+//    init?(id: String, dict: [String: Any]) {
+//        self.id = id
+//        guard let addr = dict["address"], let name = dict["name"] else {return nil}
+//        
+//    }
+//}
