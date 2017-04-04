05b04b0dd9e6759c426a34f2fea693e425d6800b
Michael Florez <mike_florez@ymail.com>
2017-04-04 13:11:57 -0400

    create graphical elements folder w/ buttons file

---
+++ TeamOrange/TeamOrange/Buttons.swift
@@ -0,0 +1,39 @@
+//
+//  Buttons.swift
+//  TeamOrange
+//
+//  Created by Michael on 4/4/17.
+//  Copyright © 2017 William Brancato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginButton: UIButton {
+    
+    let buttonImageView = UIImageView()
+    
+    init(title: String, image: UIImage, backgroundColor: UIColor) {
+        super.init(frame: CGRect.zero)
+        self.backgroundColor = backgroundColor
+        self.buttonImageView.image = image
+        self.setTitle(title, for: .normal)
+        self.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func pressed() {
+        
+    }
+    
+    func buttonImageConstraint() {
+        
+    }
+    
+}
+
+
+
--- TeamOrange/TeamOrange/LoginViewController.swift
+++ TeamOrange/TeamOrange/LoginViewController.swift
@@ -13,7 +13,6 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
