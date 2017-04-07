ba40f602b5ff988fba7ffd5c6ef788b084fece61
bgb0923 <bgb0923@gmail.com>
2017-04-07 18:22:59 -0400

    add animation for sportpickercontroller

--- TeamOrange/TeamOrange/SportPickerController.swift
+++ TeamOrange/TeamOrange/SportPickerController.swift
@@ -12,16 +12,13 @@ import UIKit
 class SportPickerController: UIViewController {
     
     let myView = SportPickerView()
-    
-    
-    
     var leadingConstraint: NSLayoutConstraint!
-    var widthConstraint: NSLayoutConstraint!
+    let button = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.buildView()
-        self.slideViewIn()
+        self.buildButton()
     }
     
     
@@ -30,30 +27,32 @@ class SportPickerController: UIViewController {
         self.myView.translatesAutoresizingMaskIntoConstraints = false
         self.myView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
         self.myView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        self.myView.leadingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.leadingConstraint = self.myView.leadingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        self.leadingConstraint.isActive = true
         self.myView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.4).isActive = true
-        
-        leadingConstraint = self.myView.leadingAnchor.constraint(equalTo: self.view.trailingAnchor)
-        leadingConstraint.isActive = true
-//
-//        widthConstraint = self.myView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.4)
-//        widthConstraint.isActive = true
         
     }
     
     func slideViewIn() {
-//        print(self.widthConstraint.constant)
-//        let width = self.view.frame.size.width * 0.4
-//        self.leadingConstraint.constant.subtract(width)
-
-        UIView.animate(withDuration: 3, animations: {
-            self.leadingConstraint.constant.subtract(-(self.view.frame.size.width * 0.4))
-//            self.myView.leadingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -(self.view.frame.size.width * 0.4)).isActive = true
-          //  self.myView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.view.layoutIfNeeded()
+        self.leadingConstraint.isActive = false
+        self.myView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        UIView.animate(withDuration: 0.5, animations: {
             self.view.layoutIfNeeded()
         }, completion: { _ in
             print("Done")
         })
+    }
+    
+    func buildButton() {
+        self.view.addSubview(button)
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.button.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
+        self.button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.2).isActive = true
+        self.button.backgroundColor = UIColor.red
+        self.button.addTarget(self, action: #selector(self.slideViewIn), for: .touchUpInside)
     }
     
 }
