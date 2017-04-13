ab6987a7a9454876970b317cff6bb26395ab6c44
bgb0923 <bgb0923@gmail.com>
2017-04-13 17:30:38 -0400

    Scroller for peak view added but empty

---
+++ TeamOrange/TeamOrange/GamePeakScroll.swift
@@ -0,0 +1,42 @@
+//
+//  GamePeakScroller.swift
+//  TeamOrange
+//
+//  Created by William Brancato on 4/13/17.
+//  Copyright © 2017 William Brancato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GamePeakScroll: UIScrollView {
+    
+//    var gameStack: UIStackView!
+//    var games: [Game] = []
+//    
+//    init() {
+//        super.init(frame: CGRect.zero)
+//        Sport.all.forEach { self.sportIcons.append($0.image) }
+//        self.setupStack()
+//        self.isPagingEnabled = true
+//        
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    func setupStack() {
+//        self.gameStack = UIStackView(arrangedSubviews: self.games)
+//    }
+    
+//    func setupStackOld() {
+//        self.sportStack = UIStackView(arrangedSubviews: self.sportIcons)
+//        self.sportStack.addAndConstrainToEdges(of: self)
+//        self.sportIcons.forEach {
+//            $0.contentMode = .scaleAspectFit
+//            $0.heightAnchor.constraint(equalTo:self.heightAnchor).isActive = true
+//            $0.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+//        }
+//    }
+}
